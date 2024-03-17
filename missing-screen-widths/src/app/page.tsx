9234fb44fcde1527288e406ee7530ac2f8d3ba4d
Alexander Bartolomey <github@alexanderbartolomey.de>
2024-03-17 14:40:54 +0100

    feat: add missing-screen-widths MWE
    Signed-off-by: Alexander Bartolomey <github@alexanderbartolomey.de>

---
+++ missing-screen-widths/src/app/page.tsx
@@ -0,0 +1,41 @@
+export default function Home() {
+  return (
+    <main className="p-4 space-y-8">
+      <div className="max-w-md">
+        <h1 className="text-xl">
+          With <code>max-w-md</code> (28rem)
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="max-w-screen-md">
+        <h1 className="text-xl">
+          With <code>max-w-screen-md</code> (should be 768px)
+        </h1>
+        <p className="font-bold text-lg">
+          Note that this size is not included by default!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+    </main>
+  );
+}
